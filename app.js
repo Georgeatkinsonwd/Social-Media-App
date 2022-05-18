62abdc6e857746db5f03a1f1bd6c03c324a58066
George Atkinson <georgeatkinsonwd@gmail.com>
2022-05-18 12:24:39 +0100

    mongodb working

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const MongoStore = require('connect-mongo')
+const session = require('express-session')
+const connectDB = require('./config/db')
+
+dotenv.config({path: './config/.env'})
+
+connectDB()
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server running on PORT ${process.env.port}`)
+})
